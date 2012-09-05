310e34ea3667ef106b06a3706d16fc91e3544990
Korstiaan de Ridder <korstiaan@korstiaan.com>
2012-09-05 19:25:59 +0200

    Move extension install script to seperate file

---
+++ bin/install_extensions.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+if [ "${MEMCACHED_VERSION:+1}" = 1 ]; then
+    wget http://pecl.php.net/get/memcached-${MEMCACHED_VERSION}.tgz
+    tar -xzf memcached-${MEMCACHED_VERSION}.tgz
+    sh -c "cd memcached-${MEMCACHED_VERSION} && phpize && ./configure && make && sudo make install"
+    echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+fi
+
+if [ "${APC_VERSION:+1}" = 1 ]; then
+    wget http://pecl.php.net/get/APC-${APC_VERSION}.tgz
+    tar -xzf APC-${APC_VERSION}.tgz
+    sh -c "cd APC-${APC_VERSION} && phpize && ./configure && make && sudo make install"
+    echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+fi
+
+if [ "${MEMCACHE_VERSION:+1}" = 1 ]; then
+    wget http://pecl.php.net/get/memcache-${MEMCACHE_VERSION}.tgz
+    tar -xzf memcache-${MEMCACHE_VERSION}.tgz
+    sh -c "cd memcache-${MEMCACHE_VERSION} && phpize && ./configure && make && sudo make install"
+    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+fi
+
+if [ "${MONGO_VERSION:+1}" = 1 ]; then
+    wget http://pecl.php.net/get/mongo-${MONGO_VERSION}.tgz
+    tar -xzf mongo-${MONGO_VERSION}.tgz
+    sh -c "cd mongo-${MONGO_VERSION} && phpize && ./configure && make && sudo make install"
+    echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"
+fi
